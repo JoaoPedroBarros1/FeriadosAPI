69aaca0924932d8a467bdd19601fca5ba0a59554
Aluno <jpe.bgi@gmail.com>
2024-04-30 11:12:54 -0300

    Changes

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState, useEffect} from 'react'
+import {View, Text, FlatList} from 'react-native'
 
 export default function App() {
+  const [feriados, setFeriados] = useState([])
+  const [country, setCountry] = useState("BR")
+
+  async function buscarFeriados() {
+    let config = {
+      methods: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+
+    let url = "http://date.nager.at/api/v3/PublicHolidays/2024/"+country
+
+    fetch(url, config)
+        .then(function(dados) {
+          return dados.json()
+        })
+        .then(function(dadosEmJson) {
+          setFeriados(dadosEmJson)
+        })
+        .catch(function(erro) {
+          alert(erro)
+        })
+  }
+
+  useEffect(() => {
+    void buscarFeriados()
+  }, [country])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Consumindo uma API</Text>
+      <Text>Listando os Feriados</Text>
+      <FlatList
+        data={feriados}
+        renderItem={({item}) => (
+          <View>
+            <Text>Nome: {item.localName}</Text>
+            <Text>Data: {item.date}</Text>
+          </View>
+        )}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
