2f67d93459b216d70f2cda0be75e79160dfeabe6
Aluno <jpe.bgi@gmail.com>
2024-04-30 14:52:21 -0300

    Atividade commit

--- App.js
+++ App.js
@@ -1,30 +1,22 @@
 import {useState, useEffect} from 'react'
-import {View, Text, FlatList} from 'react-native'
+import {View, Text, FlatList, StyleSheet, TextInput, Pressable} from 'react-native'
 
 export default function App() {
   const [feriados, setFeriados] = useState([])
+    const [input, setInput] = useState("")
   const [country, setCountry] = useState("BR")
 
   async function buscarFeriados() {
     let config = {
-      methods: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      }
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
     }
-
     let url = "http://date.nager.at/api/v3/PublicHolidays/2024/"+country
-
-    fetch(url, config)
-        .then(function(dados) {
-          return dados.json()
-        })
-        .then(function(dadosEmJson) {
-          setFeriados(dadosEmJson)
-        })
-        .catch(function(erro) {
-          alert(erro)
-        })
+    let result = await fetch(url, config)
+    let resultJson = await result.json()
+    setFeriados(resultJson)
   }
 
   useEffect(() => {
@@ -32,15 +24,26 @@ export default function App() {
   }, [country])
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Consumindo uma API</Text>
       <Text>Listando os Feriados</Text>
+        <View>
+            <TextInput
+                style={styles.input}
+                value={input}
+                placeholder={"País (BR, GB, etc)"}
+                onChangeText={setInput}
+            />
+            <Pressable style={styles.button} onPress={() => setCountry(input)}>
+                <Text style={{textAlign: "center"}}>Enviar</Text>
+            </Pressable>
+        </View>
       <FlatList
         data={feriados}
         renderItem={({item}) => (
-          <View>
-            <Text>Nome: {item.localName}</Text>
-            <Text>Data: {item.date}</Text>
+          <View style={styles.view}>
+            <Text style={styles.text}>Nome: {item.localName}</Text>
+            <Text style={styles.text}>Data: {item.date}</Text>
           </View>
         )}
       />
@@ -49,10 +52,36 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        marginTop: 25
+    },
+
+    view: {
+        flex: 1,
+        margin: 10,
+        padding: 10,
+        borderRadius: 10,
+        backgroundColor: '#00e1ff'
+    },
+
+    text: {
+        color: 'white'
+    },
+
+    input: {
+        margin: 10,
+        height: 40,
+        borderWidth: 1,
+        borderRadius: 10,
+        padding: 10
+    },
+
+    button: {
+        padding: 10,
+        borderRadius: 15,
+        backgroundColor: '#00e9ff'
+    }
 });
